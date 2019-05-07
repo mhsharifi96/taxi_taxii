9f55a5d65a48869bf9b7006c601c0fc180e58599
badrex <beheshti.7676@gmail.com>
2019-05-07 06:04:20 -0700

    dynamic dashboard

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,11 +1,11 @@
-1<?php
+<?php
 
 namespace App\Http\Controllers;
 
 use App\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-//use App\Post;
+use App\Post;
 use DB;
 
 class PostsController extends Controller
