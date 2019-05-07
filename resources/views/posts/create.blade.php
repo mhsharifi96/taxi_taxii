69fc000fdd49bac0cc13f0fda1c012ea79b993d8
mhsharifi96 <mh.sh7676@gmail.com>
2019-05-07 19:17:25 +0430

    add tags inputs

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,28 +5,38 @@
     <h1>درخواست تاکسی ذهنی</h1>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
     <div class="row">
-            
-        <div class="col-md-6 col-12">
-                <div class="form-group">
-                    <div class="col-sm-12">
-                        <lable for="category_id" class="control-label bold-class">دسته بندی</lable>
-                        <select class="form-control" name="category_id" id="category_id">
-                            @foreach(\App\Category::latest()->get() as $category)
-                                <option value="{{$category->id}}" > {{$category->name}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-12" style="order:1">
+            <div class="col-md-6 col-12">
                     <div class="form-group">
                         {{Form::label('title', 'موضوع')}}
+                        
                         {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'مقصد'])}}
                     </div>
                 </div>
+                <div class="col-md-6 col-12" style="order:2">
+                        <div class="form-group">
+                            {{Form::label('tag', 'تگ ها')}}
+                            {{Form::text('tag', '', ['class' => 'form-control','id' =>'form-tags-3', 'placeholder' => '#تاکسی #taxii'])}}
+                        </div>
+                    {{-- <label>Tags input with various validation:</label>
+                    <input id="form-tags-3" name="tags-3" type="text" value=""> --}}
+                    </div>
+
         
     </div>
     <div class="row">
+
+            <div class="col-md-6 col-12">
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <lable for="category_id" class="control-label bold-class">دسته بندی</lable>
+                            <select class="form-control" name="category_id" id="category_id">
+                                @foreach(\App\Category::latest()->get() as $category)
+                                    <option value="{{$category->id}}" > {{$category->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
             <div class="col-md-6 col-12">
                     <div class="form-group">
@@ -35,12 +45,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-6 col-12" style="order:2">
-                        <div class="form-group">
-                            {{Form::label('tag', 'تگ ها')}}
-                            {{Form::text('tag', '', ['class' => 'form-control', 'placeholder' => '#تاکسی #taxii'])}}
-                        </div>
-                    </div>
+
             {{--<select name="tags[]" id="tags" class="form-control" multiple>--}}
                 {{--<div class="form-group{{ $errors->has('tags') ? ' has-error' : '' }}">--}}
                     {{--{!! Form::label('tags', 'Tag', ['class' => 'col-md-2 control-label']) !!}--}}
@@ -54,12 +59,11 @@
                     {{--</div>--}}
                 {{--</div>--}}
 
-
-
-
-
-
     </div>
+
+
+
+
         
         <div class="form-group">
             {{Form::label('body', 'توضیحات')}}
