5b1f359173e75eaf80e7374bddccb11a3f6bf47e
mhsharif96 <mh.sh7676@gmail.com>
2019-07-25 15:00:59 +0430

    edit about and change banner photo

--- sursiz/mh/resources/views/pages/about.blade.php
+++ sursiz/mh/resources/views/pages/about.blade.php
@@ -4,106 +4,6 @@
 
 
 
-
-    {{--<h1 style="text-align: center; padding-top: 30px;">درباره‌ما</h1>--}}
-    {{----}}
-
-
-    {{--<div>--}}
-        {{--<div class="container ">--}}
-            {{--<div class="row">--}}
-                {{--<div class="col-md-12">--}}
-                    {{--<div class="section-title-divider"></div>--}}
-                    {{--<p class="section-description" style="text-align: center; font-weight: bold" >اولین تاکسی ذهنی در ایران</p>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="container" style="height: 300px">--}}
-            {{--<div class="row">--}}
-
-                {{--<div class="col-md-6 about-content">--}}
-                    {{--<h2 class="about-title">sursiz جایی برای شتاب دادن به ذهن </h2>--}}
-                    {{--<p class="about-text">--}}
-                        {{--در ایستگاه sursiz معتقدیم افراد زیادی در دنیای پیرامون ما، اهداف مشترکی با دیدگاه های متفاوتی دارند.--}}
-                    {{--<p class="about-text">--}}
-                        {{--اگر می خواهید در زندگی خود موفق شوید بهتر است با گرفتن یک تاکسی  ذهنی sursiz در این راه طولانی ،از تجربیات سایر مسافران این مسیر بهره ببرید.--}}
-                    {{--</p>--}}
-                    {{--<p class="about-text">--}}
-                        {{--sursiz  از دوکلمه ی  sur و siz تشکیل شده است--}}
-                        {{--sur  به معنی اسب و  siz به معنای تندرو می باشد.--}}
-                    {{--</p>--}}
-                {{--</div>--}}
-
-                {{--<div class="col-md-6" style="margin-bottom: 20px" >--}}
-                    {{--<img style="width: 100%; height: 50%;border-radius: 10px;" src="{{url('/image/mind.jpg')}}" alt="">--}}
-                {{--</div>--}}
-
-
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--<div id="team" style="margin-top: 300px">--}}
-        {{--<div class="container">--}}
-            {{--<div class="row">--}}
-                {{--<div class="col-md-12">--}}
-                    {{--<h3 class="section-title" style="text-align: center">تیم‌ما</h3>--}}
-                    {{--<div class="section-title-divider"></div>--}}
-                    {{--<p class="section-description" style="text-align: center">یک تیم فعال پشت شماست با ما در ارتباط باشید</p>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-
-            {{--<div class="row">--}}
-                {{--<div class="col-md-4">--}}
-                    {{--<div class="member">--}}
-                        {{--<div class="pic" style="text-align: center"><img src="storage/cover_images/67761_1556795721.jpg" alt=""></div>--}}
-                        {{--<h4 style="text-align: center"> سیدبدرالدین بهشتی صدر</h4>--}}
-                        {{--<span>برنامه نویس و توسعه دهنده ی وب سایت</span>--}}
-                        {{--<div class="social" style="text-align: center">--}}
-                            {{--<a href=""><i class="fa fa-twitter"></i></a>--}}
-                            {{--<a href=""><i class="fa fa-facebook"></i></a>--}}
-                            {{--<a href=""><i class="fa fa-google-plus"></i></a>--}}
-                            {{--<a href=""><i class="fa fa-linkedin"></i></a>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-
-
-                    {{--<div class="col-md-4">--}}
-                        {{--<div class="member">--}}
-                            {{--<div class="pic" style="text-align: center"><img src="storage/cover_images/67761_1556795721.jpg" alt=""></div>--}}
-                            {{--<h4 style="text-align: center">محمدحسین شریفی</h4>--}}
-                            {{--<span>برنامه نویس و توسعه دهنده ی وب سایت</span>--}}
-                            {{--<div class="social" style="text-align: center">--}}
-                                {{--<a href=""><i class="fa fa-twitter"></i></a>--}}
-                                {{--<a href=""><i class="fa fa-facebook"></i></a>--}}
-                                {{--<a href=""><i class="fa fa-google-plus"></i></a>--}}
-                                {{--<a href=""><i class="fa fa-linkedin"></i></a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-
-
-                {{--<div class="col-md-4">--}}
-                    {{--<div class="member">--}}
-                        {{--<div class="pic" style="text-align: center"><img src="storage/cover_images/67761_1556795721.jpg" alt=""></div>--}}
-                        {{--<h4 style="text-align: center">اسماعیل بخشی</h4>--}}
-                        {{--<span>گرافیست و تولیدکننده محتوای شبکه های اجتماعی</span>--}}
-                        {{--<div class="social" style="text-align: center">--}}
-                            {{--<a href=""><i class="fa fa-twitter"></i></a>--}}
-                            {{--<a href=""><i class="fa fa-facebook"></i></a>--}}
-                            {{--<a href=""><i class="fa fa-google-plus"></i></a>--}}
-                            {{--<a href=""><i class="fa fa-linkedin"></i></a>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-
-
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
-
-
     <div id="content" style="margin-top: 150px;">
 
         <!-- Page Banner -->
@@ -128,17 +28,15 @@
                             <span class="icon"></span>
                             <p><span>اولین تاکسی ذهنی در ایران</span>
                                 <br>
-                                <h>
-                                    sursiz جایی برای شتاب دادن به ذهن
-                                </h>
+                                <h6>
+                                جمع سپاری کاها به سبک ارتباط ذهنی
+                                </h6>
                                 <br>
-                                در ایستگاه sursiz معتقدیم افراد زیادی در دنیای پیرامون ما، اهداف مشترکی با دیدگاه های متفاوتی دارند.
+                                <a href="/">سورسیز</a> از دو کلمه سور+سیز تشکیل شده است که کلمه سور به معنی اسب سرخ و کلمه سیز به معنی تندرو است.
+                            این کلمه برخلاف ظاهر کاملا فارسی است
+                                <br><br>
 
-                                اگر می خواهید در زندگی خود موفق شوید بهتر است با گرفتن یک تاکسی ذهنی sursiz در این راه طولانی ،از تجربیات سایر مسافران این مسیر بهره ببرید.
-
-                                sursiz از دوکلمه ی sur و siz تشکیل شده است sur به معنی اسب و siz به معنای تندرو می باشد.
-
-
+                                سورسیز میخواهد با شما یاد بگیرد و<a href="/">کار تیمی</a> را گشترش بدهد.مطمئن باشید اگر بهترین ایده هم داشته باشید و آن را به عنوان یک راز نگه دارید دیر یا زود ایده شما با کیفیتی بالاتر پیاده سازی می‌شود.پس تا دیر نشده است تاکسی ذهنی خود را راه اندازی کنید.فرقی ندارد راننده باشید یا مسافر مهم آن است که در راه باشید
                             </p>
                         </div>
                     </div>
@@ -168,6 +66,13 @@
                             <div class="meter nostrips marketing">
                                 <span style="width: 80%"></span>
                             </div>
+                            <p><a href="https://mbalancer.com">
+                                مشاوره کسب و کار‌های کوچک
+                            </a>
+                                <span>80%</span></p>
+                            <div class="meter nostrips marketing">
+                                <span style="width: 80%"></span>
+                            </div>
                         </div>
                     </div>
 
@@ -182,8 +87,8 @@
                                 <p>
                                     <span>سورنا</span>
                                     <br>
-                                    یه تیم تازه تاسیس با جوان های مستعد که هر روزه به دنبال گسترش حیطه ی کاری می باشد
-
+                                    یه تیم تازه تاسیس با جوان های مستعد که هر روزه به دنبال گسترش حیطه ی کاری خود میباشد. شما هم میتوانید به ما بپیوندید و با یکدیگر یک تاکسی ذهنی جهانی بسازیم
+                                    
                                 </p>
                             </div>
                         </div>
@@ -203,7 +108,7 @@
                         <div class="staff-post">
                             <div class="staff-post-content">
                                 <h5> اسماعیل بخشی</h5>
-                                <span>گرافیست و تولیدکننده محتوای شبکه های اجتماعی</span>
+                                <span>گرافیست و تولیدکننده محتوا</span>
                             </div>
                             <div class="staff-post-gal">
                                 <ul class="staf-social">
